58d1bdbe8956d433c13dd9b9eea6bbe0c5bc8019
Techin <techinseoikrathok@gmail.com>
2025-06-22 21:43:58 +0700

    feat(postgres): Copy .env file during deployment

--- script/deploy-postgresql.sh
+++ script/deploy-postgresql.sh
@@ -3,13 +3,15 @@
 
 POSTGRES_DIR="$HOME/docker-apps/postgresql"
 REPO_POSTGRES_COMPOSE_FILE="$(dirname "$0")/../postgresql/docker-compose.yml"
+REPO_POSTGRES_ENV_FILE="$(dirname "$0")/../postgresql/.env"
 
 if [ -d "$POSTGRES_DIR" ]; then
   echo "✅ PostgreSQL directory already exists. Skipping deployment."
 else
   echo "🚀 Deploying PostgreSQL to $POSTGRES_DIR..."
   mkdir -p "$POSTGRES_DIR"
-  cp "$REPO_POSTGRES_COMPOSE_FILE" "$POSTGRES_DIR/"
+  cp "$REPO_POSTGRES_COMPOSE_FILE" "$POSTGRES_DIR/docker-compose.yml"
+  cp "$REPO_POSTGRES_ENV_FILE" "$POSTGRES_DIR/.env"
 
   cd "$POSTGRES_DIR"
   docker compose up -d
